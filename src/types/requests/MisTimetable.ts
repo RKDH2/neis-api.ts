7cae401be7da36e44d7d7750be3e758e155d4a78
rkdhs <gaons120@gmail.com>
2025-03-17 19:51:09 +0900

    fix: make optional fields in MisTimetableRequest

--- src/types/requests/MisTimetable.ts
+++ src/types/requests/MisTimetable.ts
@@ -2,25 +2,25 @@ export type MisTimetableRequest = {
   /** 시도교육청코드 */
   readonly ATPT_OFCDC_SC_CODE: string;
   /** 시도교육청명 */
-  readonly ATPT_OFCDC_SC_NM?: string;
+  readonly ATPT_OFCDC_SC_NM: string;
   /** 행정표준코드 */
-  readonly SD_SCHUL_CODE: string;
+  readonly SD_SCHUL_CODE?: string;
   /** 학교명 */
-  readonly SCHUL_NM: string;
+  readonly SCHUL_NM?: string;
   /** 학년도 */
-  readonly AY: string;
+  readonly AY?: string;
   /** 학년 */
-  readonly GRADE: string;
+  readonly GRADE?: string;
   /** 주야과정명 */
-  readonly DGHT_CRSE_SC_NM: string;
+  readonly DGHT_CRSE_SC_NM?: string;
   /** 학교과정명 */
   readonly SCHUL_CRSE_SC_N?: string;
   /** 계열명 */
-  readonly ORD_SC_NM: string;
+  readonly ORD_SC_NM?: string;
   /** 학과명 */
-  readonly DDDEP_NM: string;
+  readonly DDDEP_NM?: string;
   /** 학급명 */
-  readonly CLASS_NM: string;
+  readonly CLASS_NM?: string;
   /** 수정일자 */
-  readonly LOAD_DTM: string;
+  readonly LOAD_DTM?: string;
 };
